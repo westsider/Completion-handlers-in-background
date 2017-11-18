06cdd0b46fd51e4216f9486cf7b6dbd1c46df8d8
Warren Hansen <whansen1@mac.com>
2017-11-17 17:25:32 -0800

    example: run calculations in background, update UI on main thread

--- Completion Handlers/ViewController.swift
+++ Completion Handlers/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var updateLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let block = {
+            let textupdate = "Completion block has been run" ;
+            self.updateLable(with: textupdate)
+        }
+        // do calculations in background thread
+        DispatchQueue.global(qos: .background).async {
+            for counter in 0...100 {
+                self.runCodeAndConplete(text: "Completed Network Call \(counter)", count: counter, completion: block)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func runCodeAndConplete(text: String, count: Int, completion: @escaping () -> ()) {
+        // update UI on start
+        updateLable(with: "Starting network call \(count)")
+        for i in 1...1000000 {
+            let n = i * 100
+            _ = i * n
+        }
+        // update  UI on completion
+        DispatchQueue.main.async {
+            completion()
+            self.updateLable(with: text)
+        }
     }
-
-
+    
+    // update UI on main thread
+    func updateLable(with: String) {
+        DispatchQueue.main.async {
+            print(with)
+            self.updateLable?.text =  with
+        }
+    }
 }
 
